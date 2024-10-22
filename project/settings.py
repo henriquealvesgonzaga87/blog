5590cc615eeab2326fe8e2861769cd9a5351156b
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2024-10-22 22:00:34 +0100

    updates db on setings

--- project/settings.py
+++ project/settings.py
@@ -81,9 +81,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-DATABASES = {}
-
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
